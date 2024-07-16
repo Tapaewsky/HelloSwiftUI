a4eec9726ffd410f8c2b938195953253a67d5dec
Said Tapaev <120788741+Tapaewsky@users.noreply.github.com>
2024-07-16 18:23:43 +0300

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Said Tapaev on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
